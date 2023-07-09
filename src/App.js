b0ca8ab5bdb82a0bb6d62a02a00bca2f2c1ec544
Łukasz Zawadzki <lzawadzki@ZIS.local>
2023-07-09 11:28:30 +0200

    basic front

--- src/App.js
+++ src/App.js
@@ -1,24 +1,88 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import './App.css';
+
+function Home() {
+  return (
+    <div className="page">
+      <header>
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/menu">Menu</Link></li>
+            <li><Link to="/contact">Kontakt</Link></li>
+          </ul>
+        </nav>
+      </header>
+      <section className="hero">
+        <div className="hero-content">
+          <h1>Witaj w naszej pizzerii!</h1>
+          <p>Smaczne i świeże pizze czekają na Ciebie.</p>
+          <Link to="/menu" className="btn">Zobacz menu</Link>
+        </div>
+      </section>
+      <footer>
+        <p>&copy; 2023 Pizzeria. Wszelkie prawa zastrzeżone.</p>
+      </footer>
+    </div>
+  );
+}
+
+function Menu() {
+  return (
+    <div className="page">
+      <header>
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/menu">Menu</Link></li>
+            <li><Link to="/contact">Kontakt</Link></li>
+          </ul>
+        </nav>
+      </header>
+      <section className="menu-section">
+        <h1>Menu</h1>
+        {/* Tutaj możesz dodać kod dotyczący wyświetlania menu */}
+      </section>
+      <footer>
+        <p>&copy; 2023 Pizzeria. Wszelkie prawa zastrzeżone.</p>
+      </footer>
+    </div>
+  );
+}
+
+function Contact() {
+  return (
+    <div className="page">
+      <header>
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/menu">Menu</Link></li>
+            <li><Link to="/contact">Kontakt</Link></li>
+          </ul>
+        </nav>
+      </header>
+      <section className="contact-section">
+        <h1>Kontakt</h1>
+        {/* Tutaj możesz dodać kod dotyczący wyświetlania informacji kontaktowych */}
+      </section>
+      <footer>
+        <p>&copy; 2023 Pizzeria. Wszelkie prawa zastrzeżone.</p>
+      </footer>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/menu" component={Menu} />
+        <Route path="/contact" component={Contact} />
+      </Switch>
+    </Router>
   );
 }
 
